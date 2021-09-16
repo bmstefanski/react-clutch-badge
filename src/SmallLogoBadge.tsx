ae1eee668e36dcd8010a1717df976be55256c880
Bart Stefanski <contact@bstefanski.com>
2021-09-16 19:40:43 +0200

    change clutch logo background color to currentColor

--- src/SmallLogoBadge.tsx
+++ src/SmallLogoBadge.tsx
@@ -16,10 +16,10 @@ export function SmallLogoBadge({ classNamePrefix, reviewsCount, starsCount }: Cl
       }}
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="36" viewBox="0 0 28 32" preserveAspectRatio="none">
-        <g opacity="1" fill="#152F38">
+        <g opacity="1" fill="currentColor">
           <path
             d="M21.75 22.306c-1.652 1.46-3.789 2.252-5.993 2.222-5.08 0-8.811-3.73-8.811-8.85 0-5.12 3.612-8.693 8.81-8.693 2.21-.03 4.355.747 6.034 2.183l1.111.953 4.922-4.922-1.23-1.15C23.614 1.388 19.747-.057 15.756 0 6.628 0 0 6.589 0 15.638c0 9.05 6.787 15.836 15.757 15.836 4.03.054 7.93-1.42 10.915-4.128l1.19-1.11-5-4.922-1.112.992z"
-            fill="#152F38"
+            fill="currentColor"
           ></path>
           <circle cx="15.519" cy="15.717" r="5.279" fillOpacity="1" fill="#ff4f39"></circle>
         </g>
